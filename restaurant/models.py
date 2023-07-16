1c15d0e766071db427f6c7dc69bc42d2bfd8f410
Arsenii <origingoy@gmail.com>
2023-07-16 14:28:18 +0300

    renamed model "Menu" to "MenuItem"

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 
-# Create your models here.
+
 class Booking(models.Model):
     name = models.CharField(max_length=255)
     number_of_guests = models.IntegerField(validators=[MinValueValidator(1)])
     booking_date = models.DateTimeField()
     
     
-class Menu(models.Model):
+class MenuItem(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
