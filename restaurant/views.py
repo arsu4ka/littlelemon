1cacb10379bc02e78c12680b340be4bdb98aa749
Arsenii <origingoy@gmail.com>
2023-07-16 14:28:50 +0300

    generic views for CRUD operations with MenuItem

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics
+from . import models
+from . import serializers
 
-# Create your views here.
+
 def index(request):
     return render(request, "index.html", {})
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = models.MenuItem.objects.all()
+    serializer_class = serializers.MenuItemSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.MenuItem.objects.all()
+    serializer_class = serializers.MenuItemSerializer
